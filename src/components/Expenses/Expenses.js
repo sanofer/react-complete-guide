ca62586a9380e76a32e6fe69a5ea111bf100bace
Halima Sanofer <halimasanofer@gmail.com>
2023-04-13 11:52:40 +0530

    added filter to filter the array with selected year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,27 @@
+import { useState } from "react";
+import ExpenseFilter from "./ExpenseFilter";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+  const addDateFilter = (date) => {
+    setFilteredYear(date);
+    console.log(filteredYear);
+    
+  };
   return (
     <div className="expenses">
-      {props.expenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      <ExpenseFilter onDateFilterChange={addDateFilter} />
+      {props.expenses
+        .filter((expense) => expense.date.getFullYear().toString() === filteredYear)
+        .map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
     </div>
   );
 };
